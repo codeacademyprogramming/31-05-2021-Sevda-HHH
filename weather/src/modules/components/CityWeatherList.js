a8c417141a22151580d6496cbde31bf036043331
Sevda-HHH <SevdaMh@code.edu.az>
2021-06-01 01:11:53 +0400

    WeatherList component has been updated swutch case

--- weather/src/modules/components/CityWeatherList.js
+++ weather/src/modules/components/CityWeatherList.js
@@ -11,13 +11,13 @@ export function CityWeatherList({ cityList, degree, handleRemoveFromCityList }) 
                     {cityList.map(({ data }) => {
                         switch (degree) {
                             case 'Kelvin':
-                                temp = data.main.temp * 241
+                                temp = (data.main.temp * 241).toString() + " K"
                                 break;
                             case 'Celcius':
-                                temp = data.main.temp
+                                temp = (data.main.temp).toString() + " C"
                                 break;
                             case 'Fehrenheight':
-                                temp = data.main.temp * 33.8
+                                temp = (data.main.temp * 33.8).toString() + " F"
                                 break;
 
                             default:
@@ -25,9 +25,9 @@ export function CityWeatherList({ cityList, degree, handleRemoveFromCityList }) 
                         }
                         return (
                             <>
-                                <li> <button onClick={() => handleRemoveFromCityList(data.name)}
+                                <li key={data.name}> <button onClick={() => handleRemoveFromCityList(data.name)}
                                     className="badge bg-secondary">--</button>{data.name}  <b>
-                                        {temp} C </b></li>
+                                        {temp}  </b></li>
                             </>)
                     })}
 
